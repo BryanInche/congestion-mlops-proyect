8675cb30fb3db140c66cc88f7fc1ddbb6415f820
BryanInche <BryanInche>
2024-04-11 18:47:16 +0000

    commit201

--- code/1.ingestion_data.py
+++ code/1.ingestion_data.py
@@ -22,8 +22,8 @@ datos = consultar_postgres_y_obtener_df()
 # 2.1 Definir un nombre para el archivo csv que se guardara en el Blob Storage de Azure DataLake
 datos.to_csv("datos_raw_shougang_equipo63.csv", index=False)
 
-# 3. Se Obtiene las credenciales y accesos al contanier de Azure Data Lake conection,de Azure DataLake,interfaz de Azure (Claves de acceso: Key1) (Debes la variable "connection_string" Si NO deja hacer COMMIT DEL CODIGO EN GIT,GITHUB)
-connection_string = 'DefaultEndpointsProtocol=https;AccountName=datalakemlopsd4m;AccountKey=iWT8t74/#XlqcqoR03keDVtFZPzr0PB9zDffMPaLWMUBIAjUww8uYAVkc9xRkcBtvTmUHKBvd1sB3+ASt6mGgcQ==;EndpointSuffix=core.windows.net'
+# 3. Se Obtiene las credenciales y accesos al contanier de Azure Data Lake conection,de Azure DataLake,interfaz de Azure (Claves de acceso: Key1) (Debes la #variable "connection_string" Si NO deja hacer COMMIT DEL CODIGO EN GIT,GITHUB)
+# connection_string = 'DefaultEndpointsProtocol=https;AccountName=datalakemlopsd4m;AccountKey=iWT8t74/#XlqcqoR03keDVtFZPzr0PB9zDffMPaLWMUBIAjUww8uYAVkc9xRkcBtvTmUHKBvd1sB3+ASt6mGgcQ==;EndpointSuffix=core.windows.net'
 
 # 3.1 Conectar al Blob Storage de Azure
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
@@ -37,44 +37,44 @@ with open("datos_raw_shougang_equipo63.csv", "rb") as data:
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
     blob_client.upload_blob(data)
 
-# OPCIONAL: Si deseamos consolidar o unir todos los csv(equipos) en uno solo
-# # 1.Lista para almacenar los DataFrames de cada archivo CSV
-# dfs = []
+OPCIONAL: Si deseamos consolidar o unir todos los csv(equipos) en uno solo
+# 1.Lista para almacenar los DataFrames de cada archivo CSV
+dfs = []
 
-# # 2.Nombres de los archivos CSV
-# csv_files = ["datos_raw_shougang_equipo61.csv","datos_raw_shougang_equipo65.csv","datos_raw_shougang_equipo68.csv","datos_raw_shougang_equipo126.csv",             
-#              "datos_raw_shougang_equipo199.csv","datos_raw_shougang_equipo199.csv",
-#              "datos_raw_shougang_equipo201.csv","datos_raw_shougang_equipo205.csv"]
+# 2.Nombres de los archivos CSV
+csv_files = ["datos_raw_shougang_equipo61.csv","datos_raw_shougang_equipo65.csv","datos_raw_shougang_equipo68.csv","datos_raw_shougang_equipo126.csv",             
+             "datos_raw_shougang_equipo199.csv","datos_raw_shougang_equipo199.csv",
+             "datos_raw_shougang_equipo201.csv","datos_raw_shougang_equipo205.csv"]
 
-# # 3.Leer cada archivo CSV y guardarlos en DataFrames individuales
-# for csv_file in csv_files:
-#     # Descargar el archivo CSV del Blob Storage
-#     with open(csv_file, "wb") as my_blob:
-#         blob_client = blob_service_client.get_blob_client(container=container_name, blob=csv_file)
-#         blob_data = blob_client.download_blob()
-#         blob_data.readinto(my_blob)
+# 3.Leer cada archivo CSV y guardarlos en DataFrames individuales
+for csv_file in csv_files:
+    # Descargar el archivo CSV del Blob Storage
+    with open(csv_file, "wb") as my_blob:
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=csv_file)
+        blob_data = blob_client.download_blob()
+        blob_data.readinto(my_blob)
     
-#     # Leer el archivo CSV en un DataFrame de Pandas
-#     df = pd.read_csv(csv_file)
+    # Leer el archivo CSV en un DataFrame de Pandas
+    df = pd.read_csv(csv_file)
     
-#     # Agregar el DataFrame a la lista
-#     dfs.append(df)
+    # Agregar el DataFrame a la lista
+    dfs.append(df)
 
-# # 4.Concatenar todos los DataFrames en uno solo al final
-# result_df = pd.concat(dfs, ignore_index=True)
+# 4.Concatenar todos los DataFrames en uno solo al final
+result_df = pd.concat(dfs, ignore_index=True)
 
-# # 5.Guardar los datos extraidos desde PostgreSQL
-# # 5.1 Definir un nombre para el archivo csv que se guardara en el Blob Storage de Azure DataLake
-# result_df.to_csv("datos_raw_shougang_consolidado.csv", index=False)
+# 5.Guardar los datos extraidos desde PostgreSQL
+# 5.1 Definir un nombre para el archivo csv que se guardara en el Blob Storage de Azure DataLake
+result_df.to_csv("datos_raw_shougang_consolidado.csv", index=False)
 
-# # 5.2 Conectar al Blob Storage de Azure
-# blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+# 5.2 Conectar al Blob Storage de Azure
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
-# # 5.3 Nombre del contenedor y archivo en el Blob Storage
-# container_name = "raw/proyectocongestion_raw/fuentedatos_c4m/operacion_shougang/"
-# blob_name = "datos_raw_shougang_consolidado.csv"
+# 5.3 Nombre del contenedor y archivo en el Blob Storage
+container_name = "raw/proyectocongestion_raw/fuentedatos_c4m/operacion_shougang/"
+blob_name = "datos_raw_shougang_consolidado.csv"
 
-# # 5.4 Subir el archivo CSV al Blob Storage
-# with open("datos_raw_shougang_consolidado.csv", "rb") as data:
-#     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-#     blob_client.upload_blob(data)
+# 5.4 Subir el archivo CSV al Blob Storage
+with open("datos_raw_shougang_consolidado.csv", "rb") as data:
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    blob_client.upload_blob(data)
